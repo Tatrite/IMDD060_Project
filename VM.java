4520004004781cec78b28903e6520294c31b755f
Marcade-B <benoitmarcade3@gmail.com>
2024-12-11 13:39:04 +0100

    first-commit

---
+++ VM.java
@@ -0,0 +1,54 @@
+public class VM {
+
+    // ATTRIBUTS
+
+    private String nom;
+    private String RAM;
+    private String CPU;
+    private String stockage;
+    private String pareFeu;
+    private Boolean started;
+
+    // MÉTHODES
+
+    public void 
+
+    // GETTER & SETTER
+
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getRAM() {
+        return RAM;
+    }
+    public void setRAM(String rAM) {
+        RAM = rAM;
+    }
+
+    public String getCPU() {
+        return CPU;
+    }
+    public void setCPU(String cPU) {
+        CPU = cPU;
+    }
+
+    public String getStockage() {
+        return stockage;
+    }
+    public void setStockage(String stockage) {
+        this.stockage = stockage;
+    }
+
+    public String getPareFeu() {
+        return pareFeu;
+    }
+    public void setPareFeu(String pareFeu) {
+        this.pareFeu = pareFeu;
+    }
+
+    
+}
