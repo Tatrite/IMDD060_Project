874c7c56294b7dbd41c0360e7ebcdf18cbb31b7d
Tatrite <florian.fuchs31700@gmail.com>
2024-12-11 16:12:38 +0100

    first commit since a long time

--- project/main.java
+++ project/main.java
@@ -1,6 +1,6 @@
 package project;
 public class Main {
     public static void main(String[] args){
-        System.out.println("TODO : ALL");
+        System.out.println("TODO : parfeux");
     }
 }
