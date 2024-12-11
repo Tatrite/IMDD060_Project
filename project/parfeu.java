ae99fb4561c49aa4e830b918811b67db9d17c90d
D-Fabien <fabien.delahogue@gmail.com>
2024-12-11 14:04:13 +0100

    commit

--- project/parfeu.java
+++ project/parfeu.java
@@ -1,4 +1,19 @@
+import java.util.List;
 
 public class parfeu {
+    private List<> Regle;
+
+    private String addRegle (){
+        return "";
+    }
+
+    private String removeRegle (){
+        return "";
+    }
+
+    private __str__ afficherRegle(){
+        return list.stream().forEach(System.out::println);
+        ;
+    }
 
 }
